e8931d79836a12ccff15adc7849e3ae68da5a1b3
Alexandre Gandra Lages <gandra@gmail.com>
2012-05-17 12:01:38 -0400

    Jack and Jane

---
+++ dick/src/main/java/hello/World.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println(new Hello().getMessage());
+    }
+}
---
+++ jane/src/main/java/hello/Hello.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Hello {
+    public String getMessage() {
+        return "Hello, world!";
+    }
+}
