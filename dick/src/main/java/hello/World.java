e06061cabd398e34b2df7249b74905cad73cbca4
Alexandre Gandra Lages <gandra@gmail.com>
2012-05-17 12:13:01 -0400

    Have fun

--- dick/src/main/java/hello/World.java
+++ dick/src/main/java/hello/World.java
@@ -2,6 +2,6 @@ package hello;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println(new Hello().getMessage() + "!\n");
+        System.out.println(new Hello().getMessage() + "!\n Have fun with Dick and Jane.");
     }
 }
